d4d5bdfbc1292030ee4defa79d8cfe1d4fd8d6fc
YEP <younget02@naver.com>
2016-07-27 16:33:02 +0900

    코스 탭 으녕이

--- app/src/main/java/com/hanium/costamp/ImageAdapter.java
+++ app/src/main/java/com/hanium/costamp/ImageAdapter.java
@@ -12,66 +12,73 @@ import android.widget.ImageView;
  * Created by YEP on 2016-07-26.
  */
 
-    public class ImageAdapter extends BaseAdapter {
-        Context context;
+public class ImageAdapter extends BaseAdapter {
+    Context context;
+//    int width;
+//
+//    public int getWidth() {
+//        return width;
+//    }
 
-        private Integer[] mThumblds = {R.drawable.testimage1, R.drawable.testimage2,
-                R.drawable.testimage1,
-                R.drawable.testimage2, R.drawable.testimage1, R.drawable.testimage2, R.drawable.testimage1,
-                R.drawable.testimage2, R.drawable.testimage1, R.drawable.testimage2, R.drawable.testimage1,
-                R.drawable.testimage2
-        };
-
-
-        public ImageAdapter(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        public int getCount() {
-            return mThumblds.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final int pos = position;
-            ImageView imageView;
-
-            if (convertView == null) {
-                imageView = new ImageView(context);
-                imageView.setLayoutParams(new GridView.LayoutParams(250,250));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            } else {
-                imageView = (ImageView) convertView;
-            }
-
-            imageView.setImageResource(mThumblds[position]);
+    private Integer[] mThumblds = {R.drawable.testimage1, R.drawable.testimage2,
+            R.drawable.testimage1,
+            R.drawable.testimage2, R.drawable.testimage1, R.drawable.testimage2, R.drawable.testimage1,
+            R.drawable.testimage2, R.drawable.testimage1, R.drawable.testimage2, R.drawable.testimage1,
+            R.drawable.testimage2
+    };
 
 
-           imageView.setOnClickListener(new View.OnClickListener() {
-
-                public void onClick(View v) {
-                    View dialogView= (View)View.inflate(context, R.layout.fragment4_dialog,null);
-                    AlertDialog.Builder dlg = new AlertDialog.Builder(context);
-                    ImageView image10= (ImageView)dialogView.findViewById(R.id.image10);
-                    image10.setImageResource(mThumblds[pos]);
-                    dlg.setView(dialogView);
-                    dlg.setNegativeButton("닫기",null);
-                    dlg.show();
-                }
-            });
-
-
-            return imageView;
-        }
+    public ImageAdapter(Context context) {
+        this.context = context;
     }
+
+    @Override
+    public int getCount() {
+        return mThumblds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        ImageView imageView;
+//        getWidth();
+        if (convertView == null) {
+
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(2, 2, 2, 2);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumblds[position]);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                View dialogView = (View) View.inflate(context, R.layout.fragment4_dialog, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(context);
+                ImageView image10 = (ImageView) dialogView.findViewById(R.id.image10);
+                image10.setImageResource(mThumblds[pos]);
+                dlg.setView(dialogView);
+                dlg.setNegativeButton("닫기", null);
+                dlg.show();
+            }
+        });
+
+
+        return imageView;
+    }
+}
