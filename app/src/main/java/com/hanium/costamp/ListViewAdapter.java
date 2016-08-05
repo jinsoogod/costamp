658c70b8cfb3a2fc5e997d2627e0452696202d9e
YEP <younget02@naver.com>
2016-08-05 17:27:52 +0900

    끝2

--- app/src/main/java/com/hanium/costamp/ListViewAdapter.java
+++ app/src/main/java/com/hanium/costamp/ListViewAdapter.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
  * Created by YEP on 2016-07-26.
  */
 public class ListViewAdapter extends BaseAdapter {
-    Button  mBtn_like;
+    Button mBtn_like;
 
     private Context mContext = null;
     private int layout = 0;
@@ -31,7 +31,6 @@ public class ListViewAdapter extends BaseAdapter {
 
         this.inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
-
 
 
     @Override
@@ -53,18 +52,18 @@ public class ListViewAdapter extends BaseAdapter {
     }
 
 
-            @Override
-            public View getView(final int position, View convertView, ViewGroup parent) {
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
 
-                final int pos = position;
+//                final int pos = position;
 
-                // TODO Auto-generated method stub
-                if (convertView == null) {
-                    convertView = inflater.inflate(this.layout, parent, false);
+        // TODO Auto-generated method stub
+        if (convertView == null) {
+            convertView = inflater.inflate(this.layout, parent, false);
 
-                } else {
-                    ImageView course_image = (ImageView) convertView.findViewById(R.id.iv_course);
+        } else {
+            ImageView course_image = (ImageView) convertView.findViewById(R.id.iv_course);
             TextView course_info1 = (TextView) convertView.findViewById(R.id.tv_courseinfo1);
             TextView course_info2 = (TextView) convertView.findViewById(R.id.tv_courseinfo2);
             TextView course_info3 = (TextView) convertView.findViewById(R.id.tv_courseinfo3);
@@ -73,17 +72,31 @@ public class ListViewAdapter extends BaseAdapter {
             course_info1.setText(data.get(position).info1);
             course_info2.setText(data.get(position).info2);
             course_info3.setText(data.get(position).info3);
-
+//                    checked= (data.get(position).like);
         }
 
 
-        mBtn_like= (Button) convertView.findViewById(R.id.btn_like);
-
+        mBtn_like = (Button) convertView.findViewById(R.id.btn_like);
+        //
+        data.get(position).like = true;
         mBtn_like.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                String str = data.get(pos).info1 + "를 좋아요 하였습니다.";
-                Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
+
+
+                if (data.get(position).like) {
+                    String str = data.get(position).info1 + "를 좋아요 하였습니다.";
+                    Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
+                    //
+                    data.get(position).like = false;
+
+                } else if (data.get(position).like == false) {
+                    String str = data.get(position).info1 + "를 좋아요 취소하였습니다.";
+                    Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
+                    //
+                    data.get(position).like = true;
+                }
             }
         });
 
