4f87b6f212dad879d9718748c693b730478c484d
YEP <younget02@naver.com>
2016-08-05 15:14:33 +0900

    끝2

--- app/src/main/java/com/hanium/costamp/ListViewAdapter.java
+++ app/src/main/java/com/hanium/costamp/ListViewAdapter.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,6 +17,8 @@ import java.util.ArrayList;
  * Created by YEP on 2016-07-26.
  */
 public class ListViewAdapter extends BaseAdapter {
+    Button  mBtn_like;
+
     private Context mContext = null;
     private int layout = 0;
     private ArrayList<ListViewData> data = null;
@@ -24,8 +28,11 @@ public class ListViewAdapter extends BaseAdapter {
         this.mContext = c;
         this.layout = l;
         this.data = d;
+
         this.inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
+
+
 
     @Override
     public int getCount() {
@@ -46,17 +53,18 @@ public class ListViewAdapter extends BaseAdapter {
     }
 
 
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+            @Override
+            public View getView(final int position, View convertView, ViewGroup parent) {
 
 
-        final int pos = position;
+                final int pos = position;
 
-        // TODO Auto-generated method stub
-        if (convertView == null) {
-            convertView = inflater.inflate(this.layout, parent, false);
-        } else {
-            ImageView course_image = (ImageView) convertView.findViewById(R.id.iv_course);
+                // TODO Auto-generated method stub
+                if (convertView == null) {
+                    convertView = inflater.inflate(this.layout, parent, false);
+
+                } else {
+                    ImageView course_image = (ImageView) convertView.findViewById(R.id.iv_course);
             TextView course_info1 = (TextView) convertView.findViewById(R.id.tv_courseinfo1);
             TextView course_info2 = (TextView) convertView.findViewById(R.id.tv_courseinfo2);
             TextView course_info3 = (TextView) convertView.findViewById(R.id.tv_courseinfo3);
@@ -67,22 +75,21 @@ public class ListViewAdapter extends BaseAdapter {
             course_info3.setText(data.get(position).info3);
 
         }
-//
-//        Button mBtn_like = (Button) convertView.findViewById(R.id.btn_tmp);
-//        mBtn_like.setOnClickListener(mClickListener);
 
+
+        mBtn_like= (Button) convertView.findViewById(R.id.btn_like);
+
+        mBtn_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = data.get(pos).info1 + "를 좋아요 하였습니다.";
+                Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return convertView;
 
     }
 
-//    public AdapterView.OnClickListener mClickListener = new AdapterView.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            String str = data.get(position).info1 + "를 좋아요 하였습니다.";
-//            Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
-//        }
-//
-//    };
 
 }
